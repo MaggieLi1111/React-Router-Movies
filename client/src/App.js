51a09fe4117780d4431f6e18ee563c92eb3fd30d
unknown <lidanyang1111@gmail.com>
2021-11-08 13:57:25 -0800

    version1

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Route, Link } from "react-router-dom";
 
 import SavedList from './Movies/SavedList';
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -14,6 +17,7 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -24,13 +28,25 @@ export default function App () {
 
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
+    const alreadySaved = saved.find(movie => {
+      return movie.id === id
+    })
+    if(alreadySaved) return;
+    const foundMovie = movieList.find(movie => movie.id === id);
+    setSaved([...saved, foundMovie])
   };
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
+      <SavedList list={saved} />
 
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/" >
+      <MovieList movies={movieList}  />
+      </Route>
+
+      <Route path="/movies/:id" >
+        <Movie saveMovie={addToSavedList} />
+      </Route>
     </div>
   );
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { Link, useRouteMatch} from "react-router-dom";
+
+import MovieCard from "./MovieCard";
+
 export default function MovieList(props) {
   return (
     <div className="movie-list">
@@ -11,17 +15,18 @@ export default function MovieList(props) {
 }
 
 function MovieDetails(props) {
+
   const { title, director, metascore } = props.movie;
+
+  const { url } = useRouteMatch();
+
 
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
+      <Link to={`/movies/${props.movie.id}`}>
+      <MovieCard movie={props.movie} />
+      </Link>
+
     </div>
   );
 }
